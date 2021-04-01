ac2c84ec641e3eecaf4f6b33fbe3582a4a431fdb
GauthierQ <queau.gauthier@orange.fr>
2021-04-01 14:27:01 +0200

    crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:trotters_flutter_app/page/home.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
